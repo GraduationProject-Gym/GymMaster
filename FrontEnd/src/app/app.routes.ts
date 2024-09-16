3ad043ff84a6daf4fe44577cf7c200bcae4de6cd
DoaaMakhlouf <doaa.abdelaal.20@gmail.com>
2024-09-16 09:14:34 +0300

    components creation and bootstrap installation

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterationComponent } from './components/registeration/registeration.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: RegisterationComponent },
+    { path: "registeration", component: RegisterationComponent },
+    { path: "login", component: LoginComponent },
+    { path: "**", component: ErrorComponent }
+];
