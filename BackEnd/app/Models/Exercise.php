6efdf0646488e690c4d1a55d3db095412b59302a
mona-abdelmohsen <monaabdelmohsen101214@gmail.com>
2024-09-17 22:38:37 +0300

    add fillable to Exercies Model

--- BackEnd/app/Models/Exercise.php
+++ BackEnd/app/Models/Exercise.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Exercise extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'category',
+        'no_of_times',
+    ];
     public function gymClass()
     {
        return $this->belongsToMany(GymClass::class, 'class_exercies', 'exercies_id', 'class_id');
