4d4da7d269e967f42be7574457fe9439c2f242c9
sandymosaad <sandyzakher5@gmail.com>
2024-09-26 14:44:22 +0200

    add goal page for trainee

--- FrontEnd/src/app/components/trainee/goal/goal.component.ts
+++ FrontEnd/src/app/components/trainee/goal/goal.component.ts
@@ -1,20 +1,50 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-goal',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, FormsModule, CommonModule],
   templateUrl: './goal.component.html',
   styleUrl: './goal.component.css'
 })
 export class GoalComponent {
-  goal: string = 'Lose 5 kilograms';
+  goal: string | null = null;
+  showForm: boolean = true;
+  isEditing: boolean = false;
+  newGoal: string = '';
 
+  constructor() {}
 
+    // Edit the current goal
+  editGoal(): void {
+    this.showForm = true;
+    this.isEditing = true;
+    this.newGoal = this.goal!;
+  }
 
+    // Add the new goal
+  addGoal(): void {
+    this.goal = this.newGoal;
+    this.showForm = false;
+  }
 
+   // Save changes or add the goal
+  saveGoal(): void {
+    if (this.isEditing) {
+      this.goal = this.newGoal;
+    } else {
+      this.goal = this.newGoal;
+    }
+    this.showForm = false;
+    this.newGoal = '';
+  }
 
-  editGoal() {
-    // Logic to edit the goal
+    // Cancel the edit or addition
+  cancel(): void {
+    this.showForm = false;
+    this.newGoal = '';
   }
 }
