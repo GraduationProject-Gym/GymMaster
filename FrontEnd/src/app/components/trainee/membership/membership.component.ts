0c3fd176b5c978dbd0ce9283c8026255ddad7f09
DoaaMakhlouf <doaa.abdelaal.20@gmail.com>
2024-09-28 20:53:12 +0300

    update membership component to show data based on database

--- FrontEnd/src/app/components/trainee/membership/membership.component.ts
+++ FrontEnd/src/app/components/trainee/membership/membership.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { MembershipService } from '../../../services/trainee/membership/membership.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-membership',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, CommonModule],
   // providers: [MembershipService],
   templateUrl: './membership.component.html',
   styleUrl: './membership.component.css'
@@ -13,17 +14,37 @@ import { MembershipService } from '../../../services/trainee/membership/membersh
 export class MembershipComponent implements OnInit{
   
   constructor(private membershipService:MembershipService){}
+
+  memberships: any[] = [];
+  errorMessage: string | null = null;
+
   ngOnInit(): void {
+    this.errorMessage = null; // Reset the error message 
+
     this.membershipService.indexMemberships().subscribe({
-      next: (response) => {
-        console.log(response);
+      next: (response:any) => {
+        // this.memberships = response.Memberships;
+        response.Memberships.forEach((membership: any) => {
+          const type = membership.type;
+          if (!this.memberships[type]) {
+            this.memberships[type] = []; 
+          }
+          this.memberships[type].push(membership);
+        });
+        // console.log(response);
       },
       error: (error) => {
         if(error.status === 401){
-          console.log("Unauth.");
+          this.errorMessage = error.error?.message;
+          // console.log(error);
+        } else {
+          this.errorMessage = 'An unexpected error occurred. Please try again later.';
         }
-        console.log(error);
       }
     });
+  }
+  
+  capitalizeFirstLetter(string: string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   }
 }
