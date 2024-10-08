30c774c694e54e84a733f208fdd566fa9e30e28d
mona-abdelmohsen <monaabdelmohsen101214@gmail.com>
2024-10-08 16:35:05 +0300

    index all classes in admin profile

--- BackEnd/app/Policies/AttendancePolicy.php
+++ BackEnd/app/Policies/AttendancePolicy.php
@@ -42,32 +42,32 @@ class AttendancePolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Attendance $attendance): bool
-    {
-        //
-    }
+    // public function update(User $user, Attendance $attendance): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Attendance $attendance): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can delete the model.
+    //  */
+    // public function delete(User $user, Attendance $attendance): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Attendance $attendance): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can restore the model.
+    //  */
+    // public function restore(User $user, Attendance $attendance): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Attendance $attendance): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can permanently delete the model.
+    //  */
+    // public function forceDelete(User $user, Attendance $attendance): bool
+    // {
+    //     //
+    // }
 }
